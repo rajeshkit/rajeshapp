c1faf43f1dc88f9a23ed6a5092726a923b946bc9
Rajesh Nallusamy <rajesh.kit@gmail.com>
2021-11-03 17:31:04 +0530

    first commit

---
+++ src/main/java/com/rajesh/RajeshappApplication.java
@@ -0,0 +1,19 @@
+package com.rajesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RajeshappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajeshappApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String welcome() {
+		return "hello welcome";
+	}
+}
